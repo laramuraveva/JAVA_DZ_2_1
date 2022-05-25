9d1132d8287badc6a1fcc38aeda9efc203da32c1
laramuraveva <lara.chernishova@yandex.ru>
2022-05-25 15:22:43 +0300

    Files added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100;   //стоимость билета
+        int mili = 20;   //стоимость 1 мили
+        int x = ticketPrice / mili;
+        System.out.println(x);
+
+    }
+}
